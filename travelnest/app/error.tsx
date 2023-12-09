f2625c8d62090a5dba3fee68979d855a1257db43
sutharnaresh <119427704+sutharnaresh@users.noreply.github.com>
2023-12-09 22:16:51 +0530

    Update error.tsx

--- travelnest/app/error.tsx
+++ travelnest/app/error.tsx
@@ -3,17 +3,15 @@
 import { useEffect } from "react";
 import { EmptyState } from "@/app/components/EmptyState";
 
-// Define the properties for the ErrorState component
-interface ErrorStateProps {
+interface ErrorStatePropInt {
   error: Error; // Error object passed as a prop
 }
 
 // ErrorState component as a functional component
-const ErrorState: React.FC<ErrorStateProps> = ({ error }) => {
+const ErrorState: React.FC<ErrorStatePropInt> = ({ error }) => {
   // useEffect hook to log the error and potentially report it to the backend
   useEffect(() => {
     console.log("Error:", error);
-    // todo: report to backend (commented out for future implementation)
   }, [error]);
 
   // Render the EmptyState component with a specific title and subtitle
